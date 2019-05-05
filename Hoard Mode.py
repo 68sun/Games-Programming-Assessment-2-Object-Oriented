fe5200536715050105a6a8717c66ca90f888a404
68sun <1703208@brunel.ac.uk>
2019-05-05 18:39:19 +0100

    Pickup guns and ammo, restart game

--- Hoard Mode.py
+++ Hoard Mode.py
@@ -18,6 +18,7 @@ red = (249, 25, 21)
 width = 1600
 height = 900
 screen = pygame.display.set_mode((width, height))
+screenReset = pygame.display.set_mode((width, height))
 
 #Clock/time
 clock = pygame.time.Clock()
@@ -34,8 +35,14 @@ playerImg = pygame.transform.scale(playerImg, (50, 40))
 enemyImg = pygame.image.load("enemy.png")
 enemyImg= pygame.transform.scale(enemyImg, (256, 256)).convert_alpha()
 
-pistolBullet = pygame.image.load("pistol.png").convert_alpha()
+pistolBullet = pygame.image.load("bullet.png").convert_alpha()
 pistolBullet = pygame.transform.scale(pistolBullet, (5, 5))
+
+shotgunBullet = pygame.image.load("bullet.png").convert_alpha()
+shotgunBullet = pygame.transform.scale(shotgunBullet, (10, 10))
+
+flamethrowerBullet = pygame.image.load("fire.png").convert_alpha()
+flamethrowerBullet = pygame.transform.scale(flamethrowerBullet, (16, 22))
 
 medkitImg = pygame.image.load("medkit.png").convert_alpha()
 medkitImg = pygame.transform.scale(medkitImg, (64, 32))
@@ -48,7 +55,7 @@ flamethrowerPickup =pygame.transform.scale(flamethrowerPickup, (64, 32))
 
 
 
-##Classes
+###Classes
 #Player
 class Player(pygame.sprite.Sprite):
 
@@ -108,7 +115,8 @@ class Player(pygame.sprite.Sprite):
 
         
 
-
+##Enemies
+#Guard class
 class Guard(pygame.sprite.Sprite):
 
     def __init__ (self):
@@ -150,7 +158,8 @@ class Guard(pygame.sprite.Sprite):
         self.stopped = False
     
     
-
+##Pickup classes
+#Medkit class
 class medkit(pygame.sprite.Sprite):
 
     def __init__ (self):
@@ -167,6 +176,7 @@ class medkit(pygame.sprite.Sprite):
             self.kill()
 
 
+#Shotgun pickup class
 class shotPickup(pygame.sprite.Sprite):
 
     def __init__ (self):
@@ -183,6 +193,7 @@ class shotPickup(pygame.sprite.Sprite):
             self.kill()
 
 
+#Flamethrower pickup class
 class flamePickup(pygame.sprite.Sprite):
 
     def __init__ (self):
@@ -202,8 +213,8 @@ class flamePickup(pygame.sprite.Sprite):
 
     
     
-            
-        
+##Gun/ bullet classes
+#Pistol gun class        
 class Pistol(pygame.sprite.Sprite):
 
     def __init__ (self, x, y):
@@ -226,12 +237,75 @@ class Pistol(pygame.sprite.Sprite):
     def update(self):
         self.rect.centerx += self.bulletDx
         self.rect.centery += self.bulletDy
+
+        #Destroys when leaving screen
+        if self.rect.centerx < 0 or self.rect.centerx > width or self.rect.centery < 0 or self.rect.centery > height:
+            self.kill()
+        
+#Shotgun gun class
+class Shotgun(pygame.sprite.Sprite):
+
+    def __init__ (self, x, y, spread):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = shotgunBullet
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.centery = y
+        self.mousePos = pygame.mouse.get_pos()
+
+        #Holds degree for spreadout shotgun shots
+        self.spread = spread
+
+        #Sets up direction for shot
+        from math import atan2, cos, sin
+        
+        angle = atan2((self.mousePos[1] - self.rect.centery), (self.mousePos[0] - self.rect.centerx))
+        self.bulletDx = 10 * cos(angle + self.spread)
+        self.bulletDy = 10 * sin(angle + self.spread)
+
+    def update(self):
+        self.rect.centerx += self.bulletDx
+        self.rect.centery += self.bulletDy
+
+        #Destroys when leaving screen
+        if self.rect.centerx < 0 or self.rect.centerx > width or self.rect.centery < 0 or self.rect.centery > height:
+            self.kill()
+        
+#Flamethrower gun class
+class Flamethrower(pygame.sprite.Sprite):
+
+    def __init__ (self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = flamethrowerBullet
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.centery = y
+        self.mousePos = pygame.mouse.get_pos()
+
+        #Flame destruction variable
+        self.flameTime = time.time()
         
 
-    
+        #Sets up direction for shot
+        from math import atan2, cos, sin
         
+        angle = atan2((self.mousePos[1] - self.rect.centery), (self.mousePos[0] - self.rect.centerx))
+        self.bulletDx = 3 * cos(angle)
+        self.bulletDy = 3 * sin(angle)
 
+    def update(self):
+        self.rect.centerx += self.bulletDx
+        self.rect.centery += self.bulletDy
 
+        #Destroys when leaving screen
+        if self.rect.centerx < 0 or self.rect.centerx > width or self.rect.centery < 0 or self.rect.centery > height:
+            self.kill()
+
+        #Destroys after certain amount of time
+        if time.time() - self.flameTime > 1:
+            self.kill()
 
         
 
@@ -263,10 +337,17 @@ for i in range(6):
 bullets = pygame.sprite.Group()
 bulletTime = time.time()
 
+#Gun type variable
+gunType = 1
+
+#Ammo
+shotgunAmmo = 0
+flameAmmo = 0
+
 
 ##Pickups
 #Pickup timer
-pickupTime = time.time() - 0.5
+pickupTime = time.time() - 10
 
 #Medkit
 medkits = pygame.sprite.Group()
@@ -292,6 +373,7 @@ while True:
 
     #Draw
     screen.fill(blue)
+    screenReset.fill(blue)
     all_sprites_list.draw(screen)
 
     #Health
@@ -301,10 +383,41 @@ while True:
     elif health <= 25 and health > 0:
         pygame.draw.rect(screen, red, Rect(5, 30, health * 2, 15), 0)
 
+    #Game end
     elif health <= 0:
         all_sprites_list.remove(player)
         screen.blit(font.render("GAME OVER", True, (90,255,255)), (350,200))
         screen.blit(font.render("SCORE: " + str(score), True, (90,255,255)), (350,250))
+        screen.blit(font.render("PRESS R TO RESTART", True, (90,255,255)), (350,300))
+
+        ##Restart game
+        keystate = pygame.key.get_pressed()
+
+        #Press r to restart
+        if keystate[K_r]:
+
+            #Sprite group resets
+            all_sprites_list.empty()
+            all_sprites_list.clear(screen, screenReset)
+
+            #Resets enemies
+            for i in range(6):
+                g = Guard()
+                enemies.add(g)
+                all_sprites_list.add(g)
+
+            
+
+            #Variable resets
+            player = Player()
+            all_sprites_list.add(player)
+            score = 0
+            health = 100
+            gunType = 1
+
+
+
+
 
     #Score
     screen.blit(font.render("Score: " + str(score), True, (255,255,255)), (5,5))
@@ -315,7 +428,7 @@ while True:
     
     
     #Fire pistol bullets
-    if mouse[0] == True and time.time() - bulletTime > 0.2:
+    if mouse[0] == True and time.time() - bulletTime > 0.2 and gunType == 1:
         bullet = Pistol(player.rect.centerx, player.rect.centery)
         bullets.add(bullet)
         all_sprites_list.add(bullet)
@@ -325,6 +438,59 @@ while True:
 
         bulletTime = time.time()
 
+
+    #Fire shotgun bullets
+    if mouse[0] == True and time.time() - bulletTime > 0.6 and gunType == 2 and shotgunAmmo > 0:
+        #List for spread of additional bullets
+        angleSpread = (-0.5, -0.25, 0, 0.25, 0.5)
+        
+
+        #Loop to shoot bullets at different angles
+        for i in angleSpread:            
+            bullet = Shotgun(player.rect.centerx, player.rect.centery, i)
+            bullets.add(bullet)
+            all_sprites_list.add(bullet)
+        
+
+        
+        shotgunAmmo -= 1
+        bulletTime = time.time()
+
+    #Fire flamethrower
+    if mouse[0] == True and time.time() - bulletTime > 0.1 and gunType == 3 and flamethrowerAmmo > 0:
+
+        bullet = Flamethrower(player.rect.centerx, player.rect.centery)
+        bullets.add(bullet)
+        all_sprites_list.add(bullet)
+        
+
+        
+        flamethrowerAmmo -= 1
+        bulletTime = time.time()
+
+
+        
+
+
+
+
+    ##Weapon rest
+    if gunType == 2 and shotgunAmmo <= 0:
+        gunType = 1
+
+    elif gunType == 3 and flamethrowerAmmo <= 0:
+        gunType = 1
+
+    ##Ammo display
+    #Shotgun
+    if gunType == 2:
+        screen.blit(font.render("Shotgun", True, (255,255,255)), (705,10))
+        screen.blit(font.render("Ammo: " + str(shotgunAmmo), True, (255,255,255)), (700,30))
+
+    #Flamethrower
+    if gunType == 3:
+        screen.blit(font.render("Flamethrower", True, (255,255,255)), (705,10))
+        screen.blit(font.render("Ammo: " + str(flamethrowerAmmo), True, (255,255,255)), (700,30))
 
     
         
@@ -356,7 +522,7 @@ while True:
         
 
     ##Player pickups
-    if time.time() - pickupTime  >= 0.5:
+    if time.time() - pickupTime  >= 10:
         #Random int to select pickup
         p = random.randint(1, 3)
 
@@ -386,6 +552,23 @@ while True:
     if heals:
         health = 100
 
+
+    #Shotgun pickup
+    spread = pygame.sprite.spritecollide(player, shotguns, True, pygame.sprite.collide_mask)
+    if spread:
+        #Change gun type and set ammo
+        gunType = 2
+        shotgunAmmo = 20
+
+     
+
+
+    #Flamethrower pickup
+    fire = pygame.sprite.spritecollide(player, flamethrowers, True, pygame.sprite.collide_mask)
+    if fire:
+        #Change gun type and set ammo
+        gunType = 3
+        flamethrowerAmmo = 100
 
         
     pygame.display.flip()
